c17020164b56fc91e2c4f6c0b5598401a5e9654b
MokshKanthaliya02 <mokshkanthaliya02@gmail.com>
2025-06-28 20:11:57 +0530

    modified forloop

--- basics/forloop.cpp
+++ basics/forloop.cpp
@@ -2,16 +2,10 @@
 using namespace std;
 
 int main() {
-
     int n;
     cout << " Enter the No.: ";
     cin >> n;
-
     for (int i = 0; i <= n; i++){
-
         cout << i << endl;
-
-    }
-    
-
+    }   
 }
